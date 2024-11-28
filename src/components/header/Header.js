3347eafe503fb9e0d6669ded265255cd72e5fb62
Yassoea <elakhalyassine10@gmail.com>
2024-11-28 14:27:40 +0100

    le button logo qui nous ramène en acceuil en cliquant

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,14 @@
 import logo from "../../assets/images/logo/LOGO.svg";
 import Nav from "../nav/Nav";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
       <figure className="header__fig">
-        <img className="logo" src={logo} alt="logo de l'agence kasa" />
+      <Link to="/" className="logo-link">
+          <img className="logo" src={logo} alt="logo de l'agence kasa" />
+        </Link>
       </figure>
       <Nav className="nav-header" />
     </header>
